0e9451d3bf68eb8bff92b4b191c940db350b1e8e
ntlk <hi@ntlk.net>
2017-01-29 10:08:47 +0000

    Show some rows and do it the ES6 way

--- js/fairislemaker.js
+++ js/fairislemaker.js
@@ -1,5 +1,25 @@
-var FairIsle = React.createClass({
-  render: () => {
-    return <h1>Hi</h1>;
+class FairIsle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pattern: "this is the pattern" };
   }
-});
+
+  render() {
+    return <Repeat rows={this.props.rows} stitches={this.props.stitches} pattern={this.state.pattern} />;
+  }
+}
+
+class Repeat extends React.Component {
+  render() {
+
+    var rows = [];
+    for (var row = 0; row < this.props.rows; row++) {
+      rows.push(<li>a row</li>)
+    }
+    return (
+      <ol>
+        {rows}
+      </ol>
+    )
+  }
+}
